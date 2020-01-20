fe99672fadfcf783aaa2c8be93c5c642885d0ed9
HASAN <gmmhasan2017@gmail.com>
2020-01-20 16:44:58 +0600

    Add Appbar layout

--- app/src/main/java/com/example/navigationcomponent/MainActivity.java
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.navigationcomponent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController);
+
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController.navigateUp();
+        return super.onSupportNavigateUp();
+
+
     }
 }
